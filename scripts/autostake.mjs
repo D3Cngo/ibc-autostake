1c9d7fbebf739621e78ea0a91c24b45a046437a7
LPX <info@shapeshift.one>
2022-03-12 01:25:27 +0900

    Update autostake.mjs

--- scripts/autostake.mjs
+++ scripts/autostake.mjs
@@ -24,6 +24,13 @@ const infoLog = console.info;
 const logLog = console.log;
 const warnLog = console.warn;
 const errorLog = console.error;
+function formatDate() {
+  const date = new Date();
+  return String(date.getHours()).padStart(2, '0')
+         + ':' + String(date.getMinutes()).padStart(2, '0')
+         + ':' + String(date.getSeconds()).padStart(2, '0')
+         + ':' + String(date.getMilliseconds()).padStart(3, '0');
+}
 
 function formatMessage(arg, type, emoji, title) {
   const copyArgs = Array.prototype.slice.call(arg);
