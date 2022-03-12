019de472a06d55766cc95ec6fd1bf5f04ff4c9ff
LPX <info@shapeshift.one>
2022-03-13 04:18:40 +0900

    fix docker

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,11 @@ function About(props) {
     <>
       <Modal show={props.show} onHide={() => props.onHide()}>
         <Modal.Header closeButton>
-          <Modal.Title>About FOXFrens Auto-Compounder</Modal.Title>
+          <Modal.Title>More ShapeShift Validators Coming Soon</Modal.Title>
         </Modal.Header>
         <Modal.Body className="small">
-          <h5>How it works</h5>
-          <p>The auto-compounder makes use of a new feature in Cosmos blockchains called Authz. This allows a validator (or any other wallet) to send certain pre-authorised transactions on your behalf.</p>
-          <p>When you authorise the compound bot by signing the Authz tx, the compound bot is authorized to send WithdrawDelegatorReward for any address, and Delegate for their own validator address. The validator cannot delegate to any other validators, and the authorisation expires automatically after four months and you can revoke at any time.</p>
+          <h5>SCP-63 - Addition of Umee, Terra, and Juno</h5>
+          <p>SCP-63 was passed by the governance process to add to the list of our growing validator list. Stay tuned for the addition of Terra and Juno!</p>
           <h5>How to use FOXFrens IBC</h5>
           <ol>
             <li>Choose a network that ShapeShift DAO is currently validating.</li>
